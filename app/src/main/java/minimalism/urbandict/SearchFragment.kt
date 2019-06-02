f8f5ea58fb13daa58021c0c3a67b781eb3e9a91f
maxterjy <maxterjy@gmail.com>
2019-06-03 02:22:49 +0700

    Refactor search fragment

--- app/src/main/java/minimalism/urbandict/SearchFragment.kt
+++ app/src/main/java/minimalism/urbandict/SearchFragment.kt
@@ -4,12 +4,15 @@ package minimalism.urbandict
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.findNavController
 import org.w3c.dom.Text
 import java.lang.StringBuilder
@@ -28,19 +31,29 @@ class SearchFragment : Fragment() {
         var outView = inflater.inflate(R.layout.fragment_search, container, false)
 
         val edtKeyword = outView.findViewById<EditText>(R.id.edt_keyword)
-        val btnSearch = outView.findViewById<Button>(R.id.btn_search)
 
-        btnSearch.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(v: View?) {
-                val key = edtKeyword.text.toString()
-                goToSearchFragment(key)
+        edtKeyword.setOnEditorActionListener(object: TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if ((actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_UNSPECIFIED) && event?.action == KeyEvent.ACTION_DOWN) {
+                    val key = edtKeyword.text.toString()
+                    goToResultFragment(key)
+                    return true
+                }
+
+                return false
             }
         })
+
+        val btnSearch = outView.findViewById<Button>(R.id.btn_search)
+        btnSearch.setOnClickListener {v ->
+            val key = edtKeyword.text.toString()
+            goToResultFragment(key)
+        }
 
         return outView
     }
 
-    fun goToSearchFragment(key: String) {
+    fun goToResultFragment(key: String) {
         view?.findNavController()?.navigate(SearchFragmentDirections.actionSearchFragmentToResultFragment(key))
     }
 }
